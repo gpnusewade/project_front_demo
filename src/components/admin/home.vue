fb993b51656431a37666699acd12a97ad4da4985
guomj <gpnusewade@users.noreply.github.com>
2019-06-05 18:04:13 +0800

    提交代码

---
+++ src/components/admin/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="admin-home-page">
+        管理员首页
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
